e970e72e99f421c484bdd9722fa52cb2cf10490a
涂名恩 <42th214@gmail.com>
2024-12-03 15:39:08 +0800

    school

---
+++ NTOU_school/0136C008_Quiz_1.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	printf("0136C008\tTu,Ming-En\n");
+	int tw[2] = { 12999, 1699 };
+	float tw_to_us = tw[0] * 0.031;
+	float tw_to_jp = tw[1] * 4.6;
+	printf("%dTWD = %0.3fUSD\n", tw[0], tw_to_us);
+	printf("%dTWD = %dJPY", tw[1], (int)tw_to_jp);
+
+    return 0;
+}
