75bb4e704ca0581b08aa1168ebb9d79126b4dc49
涂名恩 <42th214@gmail.com>
2025-02-24 15:00:51 +0800

    test

--- test.cpp
+++ test.cpp
@@ -5,11 +5,16 @@ double fahrenheitToCelsius(double fahrenheit) {
     return (fahrenheit - 32) * 5.0 / 9.0;
 }
 
+void hallo(string s) {
+    cout << "Hallo " << s << endl;
+}
+
 int main() {
     double fahrenheit;
     cout << "???????: ";
     cin >> fahrenheit;
     double celsius = fahrenheitToCelsius(fahrenheit);
     cout << "?????: " << celsius << endl;
+    hallo("world");
     return 0;
 }
