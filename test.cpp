aa5f3ae3dafc653a41a6ce71f57c15855822986f
涂名恩 <42th214@gmail.com>
2025-02-24 14:46:20 +0800

    test

--- test.cpp
+++ test.cpp
@@ -1,53 +1,15 @@
-#include <iostream>
-#include <stack>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
-public:
-    bool isValid(string s) {
-        stack<char> stk;
-
-        for(char c : s) {
-            if(c == '(' || c == '[' || c == '{') {
-                stk.push(c);
-
-            }else {
-                if(stk.empty()) {
-                    return false;
-
-                }
-
-                char top = stk.top();
-                if((c == ')' && top == '(') || (c == ']' && top == '[') || (c == '}' && top == '{')) {
-                    stk.pop();
-
-                }else {
-                    return false;
-                }
-
-            }
-
-        }
-
-        return stk.empty();
-    }
-};
+double fahrenheitToCelsius(double fahrenheit) {
+    return (fahrenheit - 32) * 5.0 / 9.0;
+}
 
 int main() {
-    Solution sol;
-    string test1 = "()";      // true
-    string test2 = "()[]{}";  // true
-    string test3 = "(]";      // false
-    string test4 = "([)]";    // false
-    string test5 = "{[]}";    // true
-    
-    cout << "HI" << endl;
-    cout << boolalpha; // Åý cout ¿é¥X true/false
-    cout << sol.isValid(test1) << endl;
-    cout << sol.isValid(test2) << endl;
-    cout << sol.isValid(test3) << endl;
-    cout << sol.isValid(test4) << endl;
-    cout << sol.isValid(test5) << endl;
-
+    double fahrenheit;
+    cout << "???????: ";
+    cin >> fahrenheit;
+    double celsius = fahrenheitToCelsius(fahrenheit);
+    cout << "?????: " << celsius << endl;
     return 0;
 }
