1230c1656a5aaef05e23bb6dc7eab81726912226
涂名恩 <42th214@gmail.com>
2025-03-10 14:16:20 +0800

    修正氣泡排序程式中的陣列型別，並優化排序邏輯

--- NTOU_school/testbubbleSort0310.cpp
+++ NTOU_school/testbubbleSort0310.cpp
@@ -3,21 +3,21 @@
 using namespace std;
 
 int main() {
-    int arr[SIZE] = {'f', 'w', 's', 'o', 'a'};
+    char arr[SIZE] = {'f', 'w', 's', 'o', 'a'}; // 修改為 char 型別
     for(int i = 0; i < SIZE; i++) {
         cout << arr[i] << " ";
     }
 
     cout << endl;
-    for(int i = 0; i < SIZE; i++) {
-        for(int j = 1; j < SIZE; j++) {
-            if(arr[j] < arr[j - 1]) {
-                swap(arr[j], arr[j - 1]);
+    for(int i = 0; i < SIZE - 1; i++) {
+        for(int j = 0; j < SIZE - i - 1; j++) {
+            if(arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
             }
         }
     }
 
     for(int i = 0; i < SIZE; i++) {
-        cout << char(arr[i]) << " ";
+        cout << arr[i] << " "; 
     }
 }
