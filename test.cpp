596986a5e5e1af631de643835a65004b15ddd485
涂名恩 <42th214@gmail.com>
2025-03-20 22:16:18 +0800

    新增函數以找出陣列中的最大值和最小值

--- test.cpp
+++ test.cpp
@@ -1,20 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-1
-double fahrenheitToCelsius(double fahrenheit) {
-    return (fahrenheit - 32) * 5.0 / 9.0;
-}
 
-void hallo(string s) {
-    cout << "Hallo " << s << endl;
+void largest(int a[], int n, int *largest, int *smallest) {
+    *largest = INT_MIN;
+    *smallest = INT_MAX;
+    for (int i = 0; i < n; i++) {
+        if (a[i] > *largest) {
+            *largest = a[i];
+        }
+        if (a[i] < *smallest) {
+            *smallest = a[i];
+        }
+    }
 }
 
 int main() {
-    double fahrenheit;
-    cout << "請輸入華氏溫度: ";
-    cin >> fahrenheit;
-    double celsius = fahrenheitToCelsius(fahrenheit);
-    cout << "攝氏溫度為: " << celsius << endl;
-    hallo("world");
+    int a[] = {1, 2, 3, 4, 5};
+    int n = sizeof(a) / sizeof(a[0]);
+    int largest_num, smallest_num;
+    largest(a, n, &largest_num, &smallest_num);
+    cout << "Largest: " << largest_num << endl;
+    cout << "Smallest: " << smallest_num << endl;
     return 0;
 }
