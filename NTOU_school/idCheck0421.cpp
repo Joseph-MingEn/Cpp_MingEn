03560df046bd41390ea7fd04e033157824b1f8dd
涂名恩 <125283161+Joseph-MingEn@users.noreply.github.com>
2025-04-21 16:08:09 +0800

    Update idCheck0421.cpp

--- NTOU_school/idCheck0421.cpp
+++ NTOU_school/idCheck0421.cpp
@@ -9,7 +9,7 @@ int main() {
 	string num = "0123456789";
 
 	do {
-		cout << "請輸入生日共7碼:";
+		cout << "請輸入生日共7碼(YYYMMDD):";
 		cin >> idStr;
 		if (idStr.size() != 7) {
 			cout << "請輸入正確的格式" << endl;
@@ -17,9 +17,9 @@ int main() {
 		}
 
 		if(idStr.substr(0, 6).find_first_not_of(num) == string::npos) {
-			if(stoi(idStr.substr(0, 3)) < 113) {
-				if ( stoi(idStr.substr(3, 2)) < 12) {
-					if (stoi(idStr.substr(5, 2)) < 31) {
+			if(stoi(idStr.substr(0, 3)) <= 113) {
+				if ( stoi(idStr.substr(3, 2)) <= 12) {
+					if (stoi(idStr.substr(5, 2)) <= 31) {
 						isID = true;
 						cout << "生日:" << idStr.substr(0, 3) << "年" << idStr.substr(3, 2) << "月" << idStr.substr(5, 2) << "日" << endl;
 
