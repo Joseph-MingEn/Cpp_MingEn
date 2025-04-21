bb03d13e8096c860b6ecf3c07e2dd18458e530ae
涂名恩 <125283161+Joseph-MingEn@users.noreply.github.com>
2025-04-21 15:51:03 +0800

    Update idCheck0421.cpp

--- NTOU_school/idCheck0421.cpp
+++ NTOU_school/idCheck0421.cpp
@@ -17,24 +17,24 @@ int main() {
 		}
 
 		if(idStr.substr(0, 6).find_first_not_of(num) == string::npos) {
-			if(stoi(idStr.substr(0, 2)) < 113) {
-				if ( stoi(idStr.substr(3, 4)) < 12) {
-					if (stoi(idStr.substr(5, 6)) < 31) {
+			if(stoi(idStr.substr(0, 3)) < 113) {
+				if ( stoi(idStr.substr(3, 2)) < 12) {
+					if (stoi(idStr.substr(5, 2)) < 31) {
 						isID = true;
-						cout << "生日:" << idStr.substr(0, 2) << "年" << idStr.substr(3, 4) << "月" << idStr.substr(5, 6) << "日" << endl;
+						cout << "生日:" << idStr.substr(0, 3) << "年" << idStr.substr(3, 2) << "月" << idStr.substr(5, 2) << "日" << endl;
 
 					}else {
-						cout << idStr.substr(5, 6) << endl;
+						cout << idStr.substr(5, 2) << endl;
 						cout << "日輸入錯誤" << endl;
 					}
 
 				}else {
-					cout << idStr.substr(3, 4) << endl;
+					cout << idStr.substr(3, 2) << endl;
 					cout << "月輸入錯誤" << endl;
 				}
 
 			}else {
-				cout << idStr.substr(0, 2) << endl;
+				cout << idStr.substr(0, 3) << endl;
 				cout << "年輸入錯誤" << endl;
 			}
 		}else {
